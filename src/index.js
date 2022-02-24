9d8d3733db34a7ba4f2cde24986b1cf7de2c94ff
Vsevolod Kurochka <61834247+Skadiz@users.noreply.github.com>
2022-02-24 22:37:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function reverse (n) {
-  
+    if(Math.sign(n)== -1){
+        var num = n*(-1);
+        return (
+            parseFloat(
+              num
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+            ) * Math.sign(num)
+        )  
+    }
+    
+     else return (
+        parseFloat(
+          n
+            .toString()
+            .split('')
+            .reverse()
+            .join('')
+        ) * Math.sign(n)
+    )
 }
+
